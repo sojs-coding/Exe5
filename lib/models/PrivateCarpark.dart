c0212e3be9011032536692362ee2496916b06e2a
sojs-coding <84329771+sojs-coding@users.noreply.github.com>
2022-10-13 11:01:35 +0800

    Updated the Carpark Models
    Carpark is now the super class of Private and Public carparks.
    It's still a skeleton.

---
+++ lib/models/PrivateCarpark.dart
@@ -0,0 +1,9 @@
+// ignore: file_names
+import 'Carpark.dart';
+
+class PrivateCarpark extends Carpark {
+
+  PrivateCarpark();
+
+//PublicCarpark.fromJson(String id,Map<dynamic, dynamic> parsedJson);
+}
---
+++ lib/models/PublicCarpark.dart
@@ -0,0 +1,9 @@
+// ignore: file_names
+import 'Carpark.dart';
+
+class PublicCarpark extends Carpark {
+
+  PublicCarpark();
+
+  //PublicCarpark.fromJson(String id,Map<dynamic, dynamic> parsedJson);
+}
